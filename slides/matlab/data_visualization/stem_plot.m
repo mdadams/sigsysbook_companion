42f6f1c7f9925e93940bcddd9c381cd9b66f42a7
Michael Adams <mdadams@ece.uvic.ca>
2021-09-22 07:20:15 -0700

    Update the repository with the latest versions of files.

--- slides/matlab/data_visualization/stem_plot.m
+++ slides/matlab/data_visualization/stem_plot.m
@@ -5,4 +5,4 @@ stem(n, cos(3/32 * pi * n), 'filled', 'r', 'markersize', 5);
 stem(n, 0.5*sin(1/16 * pi * n), 'filled', 'b', 'markersize', 5);
 hold off
 legend('cos(3{\pi}n/32)', 'sin({\pi}n/16)/2');
-print('output.pdf', '-dpdf', '-bestfit');
+print('output.pdf', '-dpdf', '-bestfit', '-opengl');
